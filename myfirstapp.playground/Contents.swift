59b66e66dce6d5e874f880a8059c366899318fbe
edward.gao <edward.gao@logicmonitor.com>
2017-11-21 20:15:42 +0800

    firstcommi

---
+++ myfirstapp.playground/Contents.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+let orderButton = UILabel(frame: CGRect(x:0, y:0, width:200, height:50))
+orderButton.text = "加入购物车"
+orderButton.textColor = UIColor.white
+orderButton.backgroundColor = UIColor.darkText
+orderButton.textAlignment = .center
+orderButton.layer.cornerRadius = 10
+orderButton.clipsToBounds = true
+
+orderButton
